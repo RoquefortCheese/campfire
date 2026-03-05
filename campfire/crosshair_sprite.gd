5243d9f5137bc19150cfdd04abb0cb40640f55aa
RoquefortCheese <162758682+RoquefortCheese@users.noreply.github.com>
2026-03-05 16:51:02 -0500

    Removed files from old version

--- campfire/crosshair_sprite.gd
+++
@@ -1,4 +0,0 @@
-extends Sprite2D
-
-func _ready() -> void:
-	position = get_viewport_rect().size / 2
--- campfire/stone.gd
+++
@@ -1,6 +0,0 @@
-extends StaticBody3D
-
-func create(start: Vector3, end: Vector3):
-	for axis in range(3):
-		position[axis] = (start[axis] + end[axis]) / 2
-		scale[axis] = (end[axis] - start[axis])
